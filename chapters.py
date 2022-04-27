b814b2aba38af6bf3d7e6f564cf1c9adb712fa6a
Tejas M R <totejasmr@gmail.com>
2022-04-27 08:19:10 +0530

    Update chapters.py

--- chapters.py
+++ chapters.py
@@ -7,23 +7,25 @@ from logger import log
 from scraper import collect_chapter_content
 
 def get_chapter_count(url=""):
-    driver.get(url)
-    book_name = url.removeprefix(NOVEL_URL + "/").removesuffix("/")
-    # with open(book_name + '.html', 'w', encoding='utf-8') as fp:
-    #     fp.write(driver.page_source)
-    time.sleep(1)
-    elements = driver.find_elements(by= TAG_NAME, value='a')
-    count = 0
-    for element in elements:
-        link = element.get_attribute('href')
-        if link is None:
-            continue
-        if not book_name in link:
-            continue
-        if not '/chapter' in link:
-            continue
-        count += 1
-    
+    try:
+        driver.get(url)
+        book_name = url.removeprefix(NOVEL_URL + "/").removesuffix("/")
+        # with open(book_name + '.html', 'w', encoding='utf-8') as fp:
+        #     fp.write(driver.page_source)
+        time.sleep(1)
+        elements = driver.find_elements(by= TAG_NAME, value='a')
+        count = 0
+        for element in elements:
+            link = element.get_attribute('href')
+            if link is None:
+                continue
+            if not book_name in link:
+                continue
+            if not '/chapter' in link:
+                continue
+            count += 1
+    except:
+        return get_chapter_count(url)
     return count
 
 def get_chapter_content_from_novel(url=""):
